9a48caeed020f04677b559861268ad6325b7a2ef
zakuzakukakigori <zakuzakukakigori@gmail.com>
2024-07-14 10:35:48 +0900

    １を与えて１を返す

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,5 +1,5 @@
-pub fn calculate(_n: usize) -> u64 {
-    0
+pub fn calculate(n: usize) -> u64 {
+    n.try_into().unwrap()
 }
 
 #[cfg(test)]
@@ -9,5 +9,10 @@ mod tests {
     #[test]
     fn it_yields_0_given_0() {
         assert_eq!(calculate(0), 0)
+    }
+
+    #[test]
+    fn it_yields_1_given_1() {
+        assert_eq!(calculate(1), 1)
     }
 }
