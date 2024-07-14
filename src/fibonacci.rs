736001b63b132362a7ad2e37f5a93d2416e80551
zakuzakukakigori <zakuzakukakigori@gmail.com>
2024-07-14 11:18:28 +0900

    fibonacci数列を生成する

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,5 +1,13 @@
 pub fn calculate(n: usize) -> u64 {
-    n.try_into().unwrap()
+    if n == 0 { return 0 }
+    
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
+
+    for _ in 1..n {
+        (a, b) = (b, a + b);
+    }
+    b
 }
 
 #[cfg(test)]
@@ -7,12 +15,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_yields_0_given_0() {
-        assert_eq!(calculate(0), 0)
-    }
+    fn it_yields_fibonacci_sequence() {
+        let sequence: Vec<u64> = vec![0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
 
-    #[test]
-    fn it_yields_1_given_1() {
-        assert_eq!(calculate(1), 1)
+        let result: Vec<_> = sequence
+            .iter()
+            .enumerate()
+            .map(|(i, _)| calculate(i))
+            .collect();
+
+        assert_eq!(sequence, result);
     }
 }
